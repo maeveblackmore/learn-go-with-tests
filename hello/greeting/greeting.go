ced10b8959d882c8a2371fc19bbb93296e3795d2
Maeve Blackmore <maeveblackmore@proton.me>
2024-07-13 14:52:40 +0100

    feat(greeting): add constant language prefixes

--- hello/greeting/greeting.go
+++ hello/greeting/greeting.go
@@ -2,6 +2,25 @@ package greeting
 
 import "fmt"
 
-func Greet(name string) string {
-	return fmt.Sprintf("Hello, %s!", name)
+const (
+	englishHelloPrefix  = "Hello"
+	russianHelloPrefix  = "привет"
+	japaneseHelloPrefix = "こんにちは"
+)
+
+func Greet(prefix, name string) string {
+	if name == "" {
+		name = "world"
+	}
+
+	switch prefix {
+	case "en":
+		prefix = englishHelloPrefix
+	case "ru":
+		prefix = russianHelloPrefix
+	case "jp":
+		prefix = japaneseHelloPrefix
+	}
+
+	return fmt.Sprintf("%s, %s!", prefix, name)
 }
