e142bb02eaa313a7f1660610979f1f361559b412
Maeve Blackmore <maeveblackmore@proton.me>
2024-07-13 15:05:52 +0100

    feat: add go doc comments to greet function

--- hello/greeting/greeting.go
+++ hello/greeting/greeting.go
@@ -8,6 +8,8 @@ const (
 	japaneseHelloPrefix = "こんにちは"
 )
 
+// Takes the ISO 639-1 code of a language as the first argument, and returns a greeting string using the string given as the second argument.
+// If an empty string is provided, it uses the string "world" as a placeholder.
 func Greet(prefix, name string) string {
 	if name == "" {
 		name = "world"
