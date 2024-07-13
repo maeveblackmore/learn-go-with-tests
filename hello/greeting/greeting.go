d89a874f8364a6edadd450bd710b5590d30aff87
Maeve Blackmore <maeveblackmore@proton.me>
2024-07-13 15:15:06 +0100

    refactor: put greeting prefix logic inside function

--- hello/greeting/greeting.go
+++ hello/greeting/greeting.go
@@ -8,21 +8,24 @@ const (
 	japaneseHelloPrefix = "こんにちは"
 )
 
-// Takes the ISO 639-1 code of a language as the first argument, and returns a greeting string using the string given as the second argument.
-// If an empty string is provided, it uses the string "world" as a placeholder.
-func Greet(prefix, name string) string {
-	if name == "" {
-		name = "world"
-	}
-
-	switch prefix {
-	case "en":
-		prefix = englishHelloPrefix
+func greetingPrefix(language string) (prefix string) {
+	switch language {
 	case "ru":
 		prefix = russianHelloPrefix
 	case "jp":
 		prefix = japaneseHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+// Takes the ISO 639-1 code of a language as the first argument, and returns a greeting string using the string given as the second argument.
+// If an empty string is provided, it uses the string "world" as a placeholder.
+func Greet(language, name string) string {
+	if name == "" {
+		name = "world"
 	}
 
-	return fmt.Sprintf("%s, %s!", prefix, name)
+	return fmt.Sprintf("%s, %s!", greetingPrefix(language), name)
 }
