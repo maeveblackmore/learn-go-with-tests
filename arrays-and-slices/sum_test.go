3c953c9648e9ad6d6624cb055f13059d620f08a9
Maeve Blackmore <maeveblackmore@proton.me>
2024-07-14 19:51:34 +0100

    test(arrays-and-slices): add test function for sumall

--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -1,6 +1,9 @@
 package arraysandslices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of any size", func(t *testing.T) {
@@ -11,7 +14,16 @@ func TestSum(t *testing.T) {
 		if got != want {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
-
 	})
+}
 
+func TestSumAll(t *testing.T) {
+	t.Run("multiple collections of any size", func(t *testing.T) {
+		got := SumAll([]int{1, 2, 3}, []int{1, 2, 3})
+		want := []int{6, 6}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
 }
