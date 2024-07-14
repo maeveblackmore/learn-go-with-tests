9bf3731fc3fda03387cbbef72ba40abb51a4a2c6
Maeve Blackmore <maeveblackmore@proton.me>
2024-07-14 20:11:56 +0100

    feat(arrays-and-slices): add sumalltails

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -8,10 +8,23 @@ func Sum(numbers []int) (total int) {
 	return
 }
 
-// Given a slice of integer slices, add up each number in the slice, and return a new slice containing the totals at that index.
+// Given a slice of integer slices, add up each number in the slice, and return a new slice containing the total at that index.
 func SumAll(slices ...[]int) (sums []int) {
 	for _, numbers := range slices {
 		sums = append(sums, Sum(numbers))
+	}
+	return
+}
+
+// Given a slice of integer slices, add up each number in the slice omitting the first index, and return a new slice containing the total at that index.
+func SumAllTails(slices ...[]int) (sums []int) {
+	for _, numbers := range slices {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:] // slice[1:] will create a new slice starting from index 1, effectively removing the first element.
+			sums = append(sums, Sum(tail))
+		}
 	}
 	return
 }
