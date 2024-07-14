fae92d5728609347e446421cd958979a077ba333
Maeve Blackmore <maeveblackmore@proton.me>
2024-07-14 15:52:17 +0100

    refactor: change workspace structure

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 	"hello/greeting"
+	"hello/integers"
 )
 
 func main() {
 	prefixLanguage := "jp"
 	firstName := "Maeve"
 	fmt.Println(greeting.Greet(prefixLanguage, firstName))
+
+	integers.Add(1, 2)
 }
