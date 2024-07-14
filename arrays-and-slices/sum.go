90fc2380612d01934d8e05e6bd570c733239a46e
Maeve Blackmore <maeveblackmore@proton.me>
2024-07-14 19:50:41 +0100

    feat(arrays-and-slices): add sumall function and annotate sum function

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -1,8 +1,17 @@
 package arraysandslices
 
+// Given a slice of integers, add up each elements are return the total.
 func Sum(numbers []int) (total int) {
 	for _, number := range numbers { // The `_` represents a blank identifier.
 		total += number
+	}
+	return
+}
+
+// Given a slice of integer slices, add up each number in the slice, and return a new slice containing the totals at that index.
+func SumAll(slices ...[]int) (sums []int) {
+	for _, numbers := range slices {
+		sums = append(sums, Sum(numbers))
 	}
 	return
 }
