28d277944cf48b4e7f9eec6ef08184b76a09ac03
Maeve Blackmore <maeveblackmore@proton.me>
2024-07-15 10:45:57 +0100

    feat(structs-methods-interfaces): create methods for calculating rectangle perimeter and circumference of circle

--- structs-methods-interfaces/shapes.go
+++ structs-methods-interfaces/shapes.go
@@ -15,6 +15,10 @@ func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
+}
+
 type Circle struct {
 	Radius float64
 }
@@ -23,6 +27,6 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+func (c Circle) Circumference() float64 {
+	return 2 * math.Pi * c.Radius
 }
