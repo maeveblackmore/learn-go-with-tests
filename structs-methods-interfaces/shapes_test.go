405b53245c7cacf0a2af1e68b745f745cec1a33d
Maeve Blackmore <maeveblackmore@proton.me>
2024-07-15 13:46:46 +0100

    test(structs-methods-interfaces): refactor tests using named fields in initialisation

--- structs-methods-interfaces/shapes_test.go
+++ structs-methods-interfaces/shapes_test.go
@@ -4,38 +4,44 @@ import "testing"
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, 314.1592653589793},
-		{Triangle{12, 6}, 36.0},
+		{name: "Rectangle", shape: Rectangle{12, 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{12, 6}, hasArea: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
 
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
-		}
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea) // `%#v` prints the value in Go-syntax format.
+			}
+		})
 	}
 }
 
 func TestPerimeter(t *testing.T) {
 	perimeterTests := []struct {
-		shape Shape
-		want  float64
+		name         string
+		shape        Shape
+		hasPerimeter float64
 	}{
-		{Rectangle{32.0, 24.0}, 112.0},
-		{Circle{12.0}, 75.39822368615503},
-		{Triangle{20.0, 40.0}, 100.0},
+		{name: "Rectangle", shape: Rectangle{32.0, 24.0}, hasPerimeter: 112.0},
+		{name: "Circle", shape: Circle{12.0}, hasPerimeter: 75.39822368615503},
+		{name: "Triangle", shape: Triangle{20.0, 40.0}, hasPerimeter: 100.0},
 	}
 
 	for _, tt := range perimeterTests {
-		got := tt.shape.Perimeter()
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Perimeter()
 
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
-		}
+			if got != tt.hasPerimeter {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasPerimeter)
+			}
+		})
 	}
 }
