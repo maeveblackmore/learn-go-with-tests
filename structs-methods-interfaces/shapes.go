9e249629a5f202efe7473a08ff480d1dfefcbc65
Maeve Blackmore <maeveblackmore@proton.me>
2024-07-15 10:33:13 +0100

    feat(structs-methods-interfaces): create a struct for each shape and bind methods to them

--- structs-methods-interfaces/shapes.go
+++ structs-methods-interfaces/shapes.go
@@ -1,14 +1,28 @@
 package structsmethodsinterfaces
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+// A method is a function with a receiver, using the syntax `func (receiverName ReceiverType) MethodName(args)`.
+// In this case, `(r Rectangle)` is our receiver, which means the method `Area()` is associated with the Rectangle type.
+// The receiver name `r` allows the method to access the fields of that `Rectangle` instance.
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
