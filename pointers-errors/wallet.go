7423d64a751ce183e8ba4df3e73896eaba28f84d
Maeve Blackmore <maeveblackmore@proton.me>
2024-07-16 11:14:59 +0100

    docs(pointers-errors): add annotations

--- pointers-errors/wallet.go
+++ pointers-errors/wallet.go
@@ -6,6 +6,10 @@ type Wallet struct {
 	balance int
 }
 
+// Calling a function or method creates a copy of the arguments.
+// Without defining a pointer to the `Wallet` struct using `*`, we are modifying this copy rather than the argument passed.
+// The balance field will then never be changed, and always return the default value.
+
 func (w *Wallet) Deposit(amount int) {
 	w.balance += amount
 }
