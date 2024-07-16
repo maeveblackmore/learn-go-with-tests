57493833e2e3a78c13f1a11f41bb759e2aefdce0
Maeve Blackmore <maeveblackmore@proton.me>
2024-07-16 11:07:04 +0100

    test(pointers-errors): wallet test function

---
+++ pointers-errors/wallet_test.go
@@ -0,0 +1,15 @@
+package pointerserrors
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+	wallet.Deposit(10)
+
+	got := wallet.Balance()
+	want := 10
+
+	if got != want {
+		t.Errorf("got %d want %d... %#v", got, want, wallet.balance)
+	}
+}
