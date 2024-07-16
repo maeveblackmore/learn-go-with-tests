9d9fd51e2f9d7d3ac9a864f8223c7fa46e0e79f0
Maeve Blackmore <maeveblackmore@proton.me>
2024-07-16 11:34:59 +0100

    feat(pointers-errors): print memory address representation to showcase pointers

--- pointers-errors/wallet.go
+++ pointers-errors/wallet.go
@@ -1,5 +1,7 @@
 package pointerserrors
 
+import "fmt"
+
 // Lowercase fields are considered private outside of this package.
 
 type Wallet struct {
@@ -9,8 +11,10 @@ type Wallet struct {
 // Calling a function or method creates a copy of the arguments.
 // Without defining a pointer to the `Wallet` struct using `*`, we are modifying this copy rather than the argument passed.
 // The balance field will then never be changed, and always return the default value.
+// Removing the `*` and running `go test` will show that the memory addresses do not match.
 
 func (w *Wallet) Deposit(amount int) {
+	fmt.Printf("address of balance in test is %p \n", &w.balance)
 	w.balance += amount
 }
 
--- pointers-errors/wallet_test.go
+++ pointers-errors/wallet_test.go
@@ -1,12 +1,20 @@
 package pointerserrors
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
 	wallet.Deposit(10)
 
 	got := wallet.Balance()
+
+	// `&` obtains the memory address where the variable `wallet.balance` is stored.
+	// `%p` prints memory addresses in base 16 notation with a leading `0x` to represent hexadecimal.
+	fmt.Printf("address of balance in test is %p \n", &wallet.balance)
+
 	want := 10
 
 	if got != want {
