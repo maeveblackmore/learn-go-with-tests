009da5ba1800e5d5c2f79ea70724d4184689671b
Maeve Blackmore <maeveblackmore@proton.me>
2024-07-16 14:16:00 +0100

    refactor(pointers-errors): add balance assertion test function

--- pointers-errors/wallet_test.go
+++ pointers-errors/wallet_test.go
@@ -6,32 +6,30 @@ import (
 )
 
 func TestWallet(t *testing.T) {
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
-
-		got := wallet.Balance()
 
 		// `&` obtains the memory address where the variable `wallet.balance` is stored.
 		// `%p` prints memory addresses in base 16 notation with a leading `0x` to represent hexadecimal.
 		fmt.Printf("address of balance in test is %p \n", &wallet.balance)
 
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(10)}
 		wallet.Withdraw(Bitcoin(5))
-
-		got := wallet.Balance()
-		want := Bitcoin(5)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(5))
 	})
 }
