c57e2823a94472e651e78c4d83338cdd28b75886
Maeve Blackmore <maeveblackmore@proton.me>
2024-07-16 11:45:58 +0100

    feat(pointers-errors): create bitcoin type to be descriptive

--- pointers-errors/wallet.go
+++ pointers-errors/wallet.go
@@ -2,10 +2,12 @@ package pointerserrors
 
 import "fmt"
 
+type Bitcoin int
+
 // Lowercase fields are considered private outside of this package.
 
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
 // Calling a function or method creates a copy of the arguments.
@@ -13,11 +15,14 @@ type Wallet struct {
 // The balance field will then never be changed, and always return the default value.
 // Removing the `*` and running `go test` will show that the memory addresses do not match.
 
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	fmt.Printf("address of balance in test is %p \n", &w.balance)
 	w.balance += amount
 }
 
-func (w Wallet) Balance() int {
+// We don't technically need the pointer reciever here as we aren't modifiying the value on the field.
+// However, it's good convention to keep your method receiver types the same for consistency.
+
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
