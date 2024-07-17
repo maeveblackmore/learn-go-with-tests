8f1206d5e4fa970f27baa73411bf409e28dbdf97
Maeve Blackmore <maeveblackmore@proton.me>
2024-07-17 15:39:10 +0100

    test(pointers-errors): check error being returned

--- pointers-errors/wallet_test.go
+++ pointers-errors/wallet_test.go
@@ -17,22 +17,21 @@ func TestWallet(t *testing.T) {
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
-	t.Run("withdraw", func(t *testing.T) {
+	t.Run("withdraw with funds", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(10)}
-		wallet.Withdraw(Bitcoin(5))
+		err := wallet.Withdraw(Bitcoin(5))
+		assertNoError(t, err)
 		assertBalance(t, wallet, Bitcoin(5))
 	})
 
-	t.Run("withdraw insufficient funds", func(t *testing.T) {
+	t.Run("withdraw with insufficient funds", func(t *testing.T) {
 		startingBalance := Bitcoin(20)
 		wallet := Wallet{balance: startingBalance}
 		err := wallet.Withdraw(Bitcoin(100))
 
 		assertError(t, err, ErrInsufficientFunds)
 		assertBalance(t, wallet, startingBalance)
-
 	})
-
 }
 
 func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
@@ -42,6 +41,13 @@ func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
 
 	if got != want {
 		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
 	}
 }
 
