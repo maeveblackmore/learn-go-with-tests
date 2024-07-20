07a721e54f2c1e5335c3ea454df7dfbb2ac19888
Maeve Blackmore <maeveblackmore@proton.me>
2024-07-20 22:26:52 +0100

    feat: update imports and use Greet function from dependency injection

--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"fmt"
-	"hello/greeting"
-	"hello/integers"
+	"os"
+
+	dependencyInjection "github.com/maeveblackmore/learn-go-with-tests/dependency-injection"
+	"github.com/maeveblackmore/learn-go-with-tests/greeting"
+	"github.com/maeveblackmore/learn-go-with-tests/integers"
 )
 
 func main() {
 	prefixLanguage := "jp"
 	firstName := "Maeve"
 	fmt.Println(greeting.Greet(prefixLanguage, firstName))
-
+	dependencyInjection.Greet(os.Stdout, firstName)
 	integers.Add(1, 2)
 }
