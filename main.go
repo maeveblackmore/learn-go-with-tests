3c86fadd5fd6d7d15cf540b13502e5f80c08a777
Maeve Blackmore <maeveblackmore@proton.me>
2024-07-20 22:40:03 +0100

    feat: add http server using our greeter function

--- main.go
+++ main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"log"
+	"net/http"
 
 	dependencyInjection "github.com/maeveblackmore/learn-go-with-tests/dependency-injection"
 	"github.com/maeveblackmore/learn-go-with-tests/greeting"
-	"github.com/maeveblackmore/learn-go-with-tests/integers"
 )
+
+func MyGreetHandler(w http.ResponseWriter, r *http.Request) {
+	dependencyInjection.Greet(w, "world")
+}
 
 func main() {
 	prefixLanguage := "jp"
 	firstName := "Maeve"
 	fmt.Println(greeting.Greet(prefixLanguage, firstName))
-	dependencyInjection.Greet(os.Stdout, firstName)
-	integers.Add(1, 2)
+	port := 5001
+	address := fmt.Sprintf(":%d", port)
+
+	log.Fatal(http.ListenAndServe(address, http.HandlerFunc(MyGreetHandler)))
 }
