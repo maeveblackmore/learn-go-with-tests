32172931ea83fefd9ddf43a37acba6d65717719f
Maeve Blackmore <maeveblackmore@proton.me>
2024-07-20 22:05:06 +0100

    docs(dependency-injection): annotate test with comments

--- dependency-injection/greeting_test.go
+++ dependency-injection/greeting_test.go
@@ -6,9 +6,14 @@ import (
 )
 
 func TestGreet(t *testing.T) {
+	// Create an empty buffer.
 	buffer := bytes.Buffer{}
+
+	// Pass a pointer to our buffer so it can be mutated.
+	// Greet will modify this buffer so it's "Hello, <name>", where <name> is the second argument.
 	Greet(&buffer, "Chris")
 
+	// Convert our buffer to a string.
 	got := buffer.String()
 	want := "Hello, Chris"
 
